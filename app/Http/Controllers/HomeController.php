cd4b63f6f1a4db0f0394e3caa8a4f0593eea57e4
muhammad_essam <muhammadessamwork@outlook.com>
2019-11-25 00:44:06 +0200

    initial Setup

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\App;
+
+class HomeController extends Controller
+{
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function index(Request $request)
+    {
+        return view('home');
+    }
+
+
+}
