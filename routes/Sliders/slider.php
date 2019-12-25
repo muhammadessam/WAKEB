54ad2c8926badcaf0b59bfe0bcc4ce921a35ca9a
Muhammad Essam <mohammedessam699@gmail.com>
2019-12-25 19:46:43 +0200

    fix the slidercontroller

--- routes/Sliders/slider.php
+++ routes/Sliders/slider.php
@@ -4,15 +4,15 @@ use Illuminate\Support\Facades\Route;
 
 Route::prefix('/sliders')->group(function () {
 
-    Route::get('/','Slidercontroller@index')->name('sliderShowAll');
-    Route::get('/add', 'Slidercontroller@create')->name('sliderCreateView');
-    Route::post('/', 'Slidercontroller@store')->name('sliderStore');
-    Route::get('/{slider}/edit', 'Slidercontroller@edit')->name('sliderEditView');
-    Route::patch('/{slider}', 'Slidercontroller@update')->name('sliderUpdate');
-    Route::delete('/', 'Slidercontroller@delete')->name('sliderDelete');
-    Route::get('/deleted', 'Slidercontroller@showAllDeletedsliders')->name('sliderShowDeleted');
-    Route::post('/restore', 'Slidercontroller@restoreSlider')->name('restoreSlider');
-    Route::delete('/delete/foreDelete', 'Slidercontroller@forceDelete')->name('sliderForceDelete');
+    Route::get('/','SliderController@index')->name('sliderShowAll');
+    Route::get('/add', 'SliderController@create')->name('sliderCreateView');
+    Route::post('/', 'SliderController@store')->name('sliderStore');
+    Route::get('/{slider}/edit', 'SliderController@edit')->name('sliderEditView');
+    Route::patch('/{slider}', 'SliderController@update')->name('sliderUpdate');
+    Route::delete('/', 'SliderController@delete')->name('sliderDelete');
+    Route::get('/deleted', 'SliderController@showAllDeletedsliders')->name('sliderShowDeleted');
+    Route::post('/restore', 'SliderController@restoreSlider')->name('restoreSlider');
+    Route::delete('/delete/foreDelete', 'SliderController@forceDelete')->name('sliderForceDelete');
 
 //    Route::get('/{product}/show', 'ProductController@show')->name('showSingleProduct');
 
